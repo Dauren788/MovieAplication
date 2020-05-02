7b3dd4d22167ea86fa9de046bec75657f7286ab0
unknown <ddauren788@gmail.com>
2020-05-02 12:17:24 +0600

    first commit

---
+++ app/src/main/java/com/example/ma/api/Service.java
@@ -0,0 +1,21 @@
+package com.example.ma.api;
+
+import com.example.ma.model.MoviesResponse;
+import com.example.ma.model.TrailerResponse;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+
+public interface Service {
+
+    @GET("movie/popular")
+    Call<MoviesResponse> getPopularMovies(@Query("api_key") String apiKey);
+
+    @GET("movie/top_rated")
+    Call<MoviesResponse> getTopRatedMovies(@Query("api_key") String apiKey);
+
+    @GET("movie/{movie_id}/videos")
+    Call<TrailerResponse> getMovieTrailer(@Path("movie_id") int id, @Query("api_key") String apiKey);
+}
